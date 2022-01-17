10c015095d38bd0f0b79c5e491942d93a1ff3018
Jang Haemin <io@jhaemin.com>
2022-01-17 23:05:58 +0900

    enum impl

--- __through_book/enums/src/main.rs
+++ __through_book/enums/src/main.rs
@@ -13,6 +13,19 @@ enum IpAddrAllInOne {
     V6(String),
 }
 
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+        //
+    }
+}
+
 fn main() {
     let four = IpAddrKind::V4;
     let six = IpAddrKind::V6;
@@ -29,4 +42,7 @@ fn main() {
 
     let home2 = IpAddrAllInOne::V4(String::from("127.0.0.1"));
     let loopback2 = IpAddrAllInOne::V6(String::from("::1"));
+
+    let m = Message::Write(String::from("hello"));
+    m.call();
 }
