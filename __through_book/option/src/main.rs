b52666e36388d2cd4490f9235de7bf90db39d5b9
Jang Haemin <io@jhaemin.com>
2022-01-17 23:44:14 +0900

    Option

---
+++ __through_book/option/src/main.rs
@@ -0,0 +1,22 @@
+enum Coin {
+    Penny,
+    Nickle,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickle => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
+fn main() {
+    let some_number = Some(5);
+    let some_string = Some("a string");
+
+    let absent_number: Option<i32> = None;
+}
