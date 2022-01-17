a3a943bf7c6f165e1bf716afeca284b191e47a39
Jang Haemin <io@jhaemin.com>
2022-01-17 22:57:13 +0900

    Enum

---
+++ __through_book/enums/src/main.rs
@@ -0,0 +1,32 @@
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+enum IpAddrAllInOne {
+    V4(String),
+    V6(String),
+}
+
+fn main() {
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
+    let home2 = IpAddrAllInOne::V4(String::from("127.0.0.1"));
+    let loopback2 = IpAddrAllInOne::V6(String::from("::1"));
+}
