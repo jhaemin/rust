37e11913766939ef2ba61e4eb77092a913082a32
Jang Haemin <io@jhaemin.com>
2022-01-20 23:20:37 +0900

    match with Some and None

--- __through_book/option/src/main.rs
+++ __through_book/option/src/main.rs
@@ -22,7 +22,14 @@ fn value_in_cents(coin: Coin) -> u32 {
         Coin::Quarter(state) => {
             println!("State quarter from {:?}!", state);
             25
-        },
+        }
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
     }
 }
 
@@ -33,4 +40,8 @@ fn main() {
     let absent_number: Option<i32> = None;
 
     value_in_cents(Coin::Quarter(UsState::Alaska));
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
 }
