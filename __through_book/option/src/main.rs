27045948336affa1d9021db6f7aec4a6a222b7fb
Jang Haemin <io@jhaemin.com>
2022-01-21 22:31:42 +0900

    match _

--- __through_book/option/src/main.rs
+++ __through_book/option/src/main.rs
@@ -44,4 +44,13 @@ fn main() {
     let five = Some(5);
     let six = plus_one(five);
     let none = plus_one(None);
+
+    let some_u8_value = 0u8;
+    match some_u8_value {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => (),
+    }
 }
