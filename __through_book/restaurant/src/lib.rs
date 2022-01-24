d7090dae125eaccc1a3a293d9427e229d5a3006f
Jang Haemin <io@jhaemin.com>
2022-01-24 22:17:55 +0900

    super

--- __through_book/restaurant/src/lib.rs
+++ __through_book/restaurant/src/lib.rs
@@ -30,3 +30,14 @@ pub fn eat_at_restaurant() {
     // Relative path
     front_of_house::hosting::add_to_waitlist();
 }
+
+fn serve_order() {}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::serve_order();
+    }
+
+    fn cook_order() {}
+}
