246fb1c405d052d2b0b09eedaa18eac453567cbd
Jang Haemin <io@jhaemin.com>
2022-01-26 23:56:54 +0900

    Add Breakfast struct and impl

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -29,4 +29,18 @@ mod back_of_house {
     }
 
     fn cook_order() {}
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("Peach"),
+            }
+        }
+    }
 }
