7fba6ebd1c70e33ba489e0926358711847ee8999
Jang Haemin <io@jhaemin.com>
2022-01-26 23:48:59 +0900

    super & reformat

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,21 +1,32 @@
 mod front_of_house {
-  pub mod hosting {
-    pub fn add_to_waitlist() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
 
-    fn seat_at_table() {}
-  }
+        fn seat_at_table() {}
+    }
 
-  mod serving {
-    fn take_order() {}
+    mod serving {
+        fn take_order() {}
 
-    fn serve_order() {}
+        fn serve_order() {}
 
-    fn take_payment() {}
-  }
+        fn take_payment() {}
+    }
 }
 
 pub fn eat_at_restaurant() {
-  crate::front_of_house::hosting::add_to_waitlist();
+    crate::front_of_house::hosting::add_to_waitlist();
 
-  front_of_house::hosting::add_to_waitlist();
+    front_of_house::hosting::add_to_waitlist();
+}
+
+fn serve_order() {}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::serve_order();
+    }
+
+    fn cook_order() {}
 }
