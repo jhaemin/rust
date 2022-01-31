d580965daa899b4e19c989c0a8b212346fe18d9a
Jang Haemin <io@jhaemin.com>
2022-01-31 09:37:48 +0900

    example 7-16 (as keyword)

--- __through_book/restaurant/src/lib.rs
+++ __through_book/restaurant/src/lib.rs
@@ -92,10 +92,5 @@ fn hashMapTest() {
 use std::fmt;
 use std::io;
 
-fn function1() -> fmt::Result {
-    //
-}
-
-fn function2() -> io::Result<()> {
-    //
-}
+use std::fmt::Result;
+use std::io::Result as IoResult;
